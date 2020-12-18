7d37ea6b41520a0bb41a5a8a17a26d1096df5804
Olybear9 <olybear9@gmail.com>
2020-12-18 16:17:38 -0600

    chore: Cleanup

--- commands/Ping.ts
+++ commands/Ping.ts
@@ -2,19 +2,7 @@ import { Client, Command, Message } from "../lib/assistant/mod.ts";
 
 export default class Ping extends Command {
 	public constructor() {
-		 super(
-			  'Ping',
-			  'ping',
-			  'Gets the response time from the bot to discord',
-			  {
-				   cooldown: 0,
-				   disabledEvents: [],
-				   argOptions: {
-						api: 3,
-						wrap: false
-				   }
-			  }
-		 );
+		 super('PingCommand', 'ping', 'Gets the response time from the bot to discord');
 	}
 
 	public async onRun(bot: Client, msg: Message): Promise<void> {
