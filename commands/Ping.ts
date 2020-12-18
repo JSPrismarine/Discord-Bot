ac6caf4ccbd0a576ba11c06e4a410cdf279f065d
Olybear9 <olybear9@gmail.com>
2020-12-18 16:37:31 -0600

    chore: Fix ping command

--- commands/Ping.ts
+++ commands/Ping.ts
@@ -2,11 +2,13 @@ import { Client, Command, Message } from "../lib/assistant/mod.ts";
 
 export default class Ping extends Command {
 	public constructor() {
-		 super('PingCommand', 'ping', 'Gets the response time from the bot to discord');
+		super('PingCommand', 'ping', 'Gets the response time from the bot to discord');
 	}
 
 	public async onRun(bot: Client, msg: Message): Promise<void> {
-		 const m: Message = await msg.reply("Pinging...");
-		 m.edit(`Pong \`${Date.now() - m.createdAt}\` ms!`);
+		let before = Date.now();
+		const m: Message = await msg.reply("Pinging...");
+		let now = Date.now();
+		await m.edit(`Pong \`${now - before}\` ms!`);
 	}
 }
