cac888a181fe378f207fd941ef84253bb5dc4cda
Olybear9 <olybear9@gmail.com>
2020-12-18 16:27:28 -0600

    chore: Make use of .env

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A discord bot for jsp written in typescript with deno using a custom written framework.
 > The framework is private until I finish it :)
 ## How to start?
-Run `deno run --allow-net ./mod.ts`
+Run `deno run --allow-net --allow-env --allow-read ./mod.ts`
--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import Ping from "./commands/Ping.ts";
 import { Client, CommandHandler, Module } from './lib/assistant/mod.ts';
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const bot: Client = new Client();
 const handler = new CommandHandler(bot, { prefix: "!" });
@@ -7,11 +8,9 @@ const handler = new CommandHandler(bot, { prefix: "!" });
 handler.registerModule(new Module('default', [
 	new Ping
 ]));
-
 handler.start();
-
-bot.connect(prompt('Token:') ?? "");
 
 bot.on('ready', () => {
 	console.log("Bot connected with valid session id");
 });
+bot.connect(config().DISCORD_TOKEN ?? prompt("Token:") ?? "");
